1195d20588ac1a71108d8b7e095fe2a31844824f
Ch0p1ne <souahthomad@gmail.com>
2025-03-18 10:19:18 +0000

    implementation partielle de la route csharp/save

--- src/routes/csharpapp.js
+++ src/routes/csharpapp.js
@@ -3,5 +3,19 @@ import express from 'express'
 
 var route = express.Router();
 
+route.patch('/csharp/save', (req, res) => {
+    try {
+        res.statusCode = 200;
+        res.setHeader('content-type', 'text/plain;charset=UTF-8');
+        res.end(JSON.stringify("Vous données en été correctement enregistrer en BDD"));
+
+        const jsonData = JSON.stringify(req.body);
+        console.log(jsonData);
+        
+    } catch (error) {
+        console.error(`error: ${error}`);
+    }
+})
+
 
 export const csharpapp = route;
--- src/routes/patch.js
+++ src/routes/patch.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 let router = express.Router();
 
-router.patch('/csharp/save', (req, res) => {
+router.patch('/patch', (req, res) => {
     try {
         res.statusCode = 200;
         res.setHeader('content-type', 'text/plain;charset=UTF-8');
